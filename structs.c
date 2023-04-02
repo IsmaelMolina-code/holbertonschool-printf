ac1ca0eb45bbb1c4ac795c83fc432e9a592251b1
Super User <ismaelmolina544@gmail.com>
2023-04-02 10:06:06 -0700

    Debug the null char

--- structs.c
+++ structs.c
@@ -4,19 +4,18 @@
  * print_unknown - prints a character preceded by '%' for unknown specifier
  *
  * @c: character '%'
- * @format: the format specifier that caused the error
  *
  * Return: the number of characters printed
  */
 
 int print_unknown(char c)
 {
-        int count = 0;
+	int count = 0;
 
-        write(1, &c, 1);
-        count++;
+	write(1, &c, 1);
+	count++;
 
-        return (count);
+	return (count);
 }
 
 /**
@@ -30,8 +29,8 @@ int print_unknown(char c)
 
 int print_char(va_list args)
 {
-        _putchar(va_arg(args, int));
-        return (1);
+	_putchar(va_arg(args, int));
+	return (1);
 }
 
 /**
@@ -45,27 +44,27 @@ int print_char(va_list args)
 
 int print_string(va_list args)
 {
-        char *s = va_arg(args, char *);
-        
+	char *s = va_arg(args, char *);
+
 	int len = 0;
-        
+
 	if (!s)
-        {
-                s = "(null)";
-        }
-        while (*s != '\0')
-        {
-                if (*s == '%')
-                {
-                        len += write(1, "%%", 1);
-                }
-                else
-                {
-                        len += write(1, s, 1);
-                }
-                s++;
-        }
-        return (len);
+	{
+		s = "(null)";
+	}
+	while (*s != '\0')
+	{
+		if (*s == '%')
+		{
+			len += write(1, "%%", 1);
+		}
+		else
+		{
+			len += write(1, s, 1);
+		}
+		s++;
+	}
+	return (len);
 }
 
 /**
@@ -79,7 +78,7 @@ int print_string(va_list args)
 
 int print_perc(va_list args)
 {
-        (void)args;
-        _putchar('%');
-        return (1);
+	(void)args;
+	_putchar('%');
+	return (1);
 }
